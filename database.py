5283df8525a567168bd47eb8bceaa100c45043cc
Giuseppe Canale <20385856+xbeat@users.noreply.github.com>
2025-03-04 22:53:46 +0100

    fix

--- database.py
+++ database.py
@@ -1,17 +1,33 @@
+# database.py
 import os
 import psycopg2
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
-load_dotenv()
+# Carica le variabili d'ambiente
+load_dotenv('.env')  # Per sviluppo locale
 
 class Database:
     def __init__(self):
-        self.conn = psycopg2.connect(
-            host=os.getenv('DB_HOST'),
-            database=os.getenv('DB_NAME'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD')
-        )
+        if os.getenv('RENDER'):
+            # Modalità Render (produzione)
+            db_url = urlparse(os.getenv('DATABASE_URL'))
+            self.conn = psycopg2.connect(
+                dbname=db_url.path[1:],
+                user=db_url.username,
+                password=db_url.password,
+                host=db_url.hostname,
+                port=db_url.port,
+                sslmode='require'
+            )
+        else:
+            # Modalità locale
+            self.conn = psycopg2.connect(
+                host=os.getenv('DB_HOST'),
+                database=os.getenv('DB_NAME'),
+                user=os.getenv('DB_USER'),
+                password=os.getenv('DB_PASSWORD')
+            )
     
     def save_message(self, user_id, message, response):
         with self.conn.cursor() as cur:
